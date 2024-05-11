8be6cf9727088e5c99b663475470d2ad72936556
Zota0 <77154534+Zota0@users.noreply.github.com>
2024-05-11 13:41:18 +0200

    Update skrypt.sql

--- praca/skrypt.sql
+++ praca/skrypt.sql
@@ -1,44 +1,56 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+01:00";
 
-CREATE DATABASE IF NOT EXISTS `piabd2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `piabd2`;
+CREATE DATABASE IF NOT EXISTS piabd2 DEFAULT CHARACTER SET utf8 COLLATE utf8_polish_ci;
+USE piabd2;
 
-CREATE TABLE `account` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phone` int(9) NOT NULL,
-  `birthdate` date NOT NULL
-) /*  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci; */
+CREATE TABLE ACCOUNT (
+  id INT(6) UNSIGNED NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone INT(9) NOT NULL,
+  birthdate DATE NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY username (username)
+) /* ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci */; 
 
-INSERT INTO `account` (`id`, `username`, `password`, `email`, `phone`, `birthdate`) VALUES
-(14, 'Developer', '$2y$10$MEdlgXmWTgeDDeQg3reXzeL2.S8f8QI56VPa4y29PkpomPdMbK1PO', 'zota0-dev@gmail.com', 697178618, '2009-08-20'),
-(21, 'Admin', '', 'Admin@admin.pl', 0, '1966-09-30');
+INSERT INTO ACCOUNT (
+  username,
+  password,
+  email,
+  phone,
+  birthdate
+)
+VALUES (
+  'Developer',
+  '$2y$10$MEdlgXmWTgeDDeQg3reXzeL2.S8f8QI56VPa4y29PkpomPdMbK1PO',
+  'zota0-dev@gmail.com',
+  697178618,
+  '2009-08-20'
+);
 
-CREATE TABLE `comments` (
-  `ID` int(4) NOT NULL,
-  `CREATIONDATE` datetime NOT NULL DEFAULT current_timestamp(),
-  `CONTENT` text NOT NULL,
-  `DISPLAYNAME` varchar(100) NOT NULL
-) /*  ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci; */
+CREATE TABLE comments (
+  ID INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,
+  CREATIONDATE DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  CONTENT TEXT NOT NULL,
+  DISPLAYNAME VARCHAR(100) NOT NULL,
+  PRIMARY KEY (ID)
+) /* ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci */; 
 
-INSERT INTO `comments` (`ID`, `CREATIONDATE`, `CONTENT`, `DISPLAYNAME`) VALUES
-(162, '2024-04-17 20:13:21', '<b> Pogrubiony </b>', 'Developer'),
-(163, '2024-04-17 20:14:08', '<span style=\'color:red\'> UWAGA! </span> <b> A nie...</b> jednak fałszywy alarm... :)', 'Developer');
+INSERT INTO comments (
+  CREATIONDATE,
+  CONTENT,
+  DISPLAYNAME
+)
+VALUES (
+  '2024-04-17 20:13:21',
+  '<b> Pogrubiony </b>',
+  'Developer'
+),
+(
+  '2024-04-17 20:14:08',
+  '<span style=\'color:red\'> UWAGA! </span> <b> A nie...</b> jednak fałszywy alarm... :)',
+  'Developer'
+);
 
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
-
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`ID`);
-
-ALTER TABLE `account`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
-ALTER TABLE `comments`
-  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=165;
 COMMIT;
